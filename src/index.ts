36d381cf2f77f61a0fa7d64709335e455851d536
Jason Schrader <whoabuddy@users.noreply.github.com>
2022-11-25 16:36:54 -0700

    fix: create separate router and modify headers on response

--- src/index.ts
+++ src/index.ts
@@ -7,76 +7,83 @@ export interface Env {
   sip015_index: KVNamespace;
 }
 
+async function simpleRouter(request: Request, env: Env): Promise<Response> {
+  const requestUrl = new URL(request.url);
+  const targetAddress = requestUrl.pathname.slice(1);
+  console.log(`path: ${requestUrl.pathname}`);
+
+  if (requestUrl.pathname === "/") {
+    return new Response(
+      `Simple transaction indexer for Stacks, supported routes:\n
+        /{principal} - returns all transactions for a given principal
+        /data - returns all known keys in Cloudflare KV store
+        / - returns this page`
+    );
+  }
+
+  if (requestUrl.pathname === "/data") {
+    const kvKeyList = await env.sip015_index.list();
+    return new Response(JSON.stringify(kvKeyList, null, 2));
+  }
+
+  if (validateStacksAddress(targetAddress)) {
+    // get transaction data from KV if it exists
+    const { value, metadata } = await env.sip015_index.getWithMetadata(
+      targetAddress,
+      {
+        type: "json",
+      }
+    );
+
+    // if it exists, check against current API total
+    if (value && metadata) {
+      console.log(`found in KV: ${targetAddress}`);
+      // get the current total transactions from the API
+      const apiTxList = await getAccountTxs(targetAddress, false);
+      const apiTotal = apiTxList.totalQueried;
+      // if the total is the same, return the cached data
+      if (apiTotal === (metadata as StacksTxKvMetadata).totalQueried) {
+        console.log(`totals match, responding with KV data`);
+        const txList = {
+          ...metadata,
+          results: value,
+        };
+        return new Response(JSON.stringify(txList, null, 2));
+      }
+      console.log(`totals do not match, fetching fresh data from API`);
+    }
+
+    // otherwise, fetch all TX from API and store in KV
+    const txList = await getAccountTxs(requestUrl.pathname, true);
+    await env.sip015_index.put(targetAddress, JSON.stringify(txList.results), {
+      metadata: {
+        totalProcessed: txList.totalProcessed,
+        totalQueried: txList.totalQueried,
+        totalResults: txList.totalResults,
+        lastUpdated: txList.lastUpdated,
+      },
+    });
+    return new Response(JSON.stringify(txList, null, 2));
+  }
+
+  return new Response(`${targetAddress} is not a valid Stacks address.`, {
+    status: 404,
+  });
+}
+
 export default {
   async fetch(
     request: Request,
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    const requestUrl = new URL(request.url);
-    const targetAddress = requestUrl.pathname.slice(1);
-    console.log(`path: ${requestUrl.pathname}`);
-
-    if (requestUrl.pathname === "/") {
-      return new Response(
-        `Simple transaction indexer for Stacks, supported routes:\n
-        /{principal} - returns all transactions for a given principal
-        /data - returns all known keys in Cloudflare KV store
-        / - returns this page`
-      );
-    }
-
-    if (requestUrl.pathname === "/data") {
-      const kvKeyList = await env.sip015_index.list();
-      return new Response(JSON.stringify(kvKeyList, null, 2));
-    }
-
-    if (validateStacksAddress(targetAddress)) {
-      // get transaction data from KV if it exists
-      const { value, metadata } = await env.sip015_index.getWithMetadata(
-        targetAddress,
-        {
-          type: "json",
-        }
-      );
-
-      // if it exists, check against current API total
-      if (value && metadata) {
-        console.log(`found in KV: ${targetAddress}`);
-        // get the current total transactions from the API
-        const apiTxList = await getAccountTxs(targetAddress, false);
-        const apiTotal = apiTxList.totalQueried;
-        // if the total is the same, return the cached data
-        if (apiTotal === (metadata as StacksTxKvMetadata).totalQueried) {
-          console.log(`totals match, responding with KV data`);
-          const txList = {
-            ...metadata,
-            results: value,
-          };
-          return new Response(JSON.stringify(txList, null, 2));
-        }
-        console.log(`totals do not match, fetching fresh data from API`);
-      }
-
-      // otherwise, fetch all TX from API and store in KV
-      const txList = await getAccountTxs(requestUrl.pathname, true);
-      await env.sip015_index.put(
-        targetAddress,
-        JSON.stringify(txList.results),
-        {
-          metadata: {
-            totalProcessed: txList.totalProcessed,
-            totalQueried: txList.totalQueried,
-            totalResults: txList.totalResults,
-            lastUpdated: txList.lastUpdated,
-          },
-        }
-      );
-      return new Response(JSON.stringify(txList, null, 2));
-    }
-
-    return new Response(`${targetAddress} is not a valid Stacks address.`, {
-      status: 500,
-    });
+    // return response from router
+    const response = await simpleRouter(request, env);
+    const newResponse = new Response(response.body, response);
+    newResponse.headers.set("Access-Control-Allow-Origin", "*");
+    newResponse.headers.set("Access-Control-Allow-Methods", "GET, OPTIONS");
+    newResponse.headers.set("Access-Control-Max-Age", "86400");
+    newResponse.headers.set("X-Stacks-Indexer", "0.0.1");
+    return newResponse;
   },
 };
